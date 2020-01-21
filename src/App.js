0de6112468152452f0550b5420d58f485d314391
Sean Yim <superjjang89@gmail.com>
2020-01-21 16:47:52 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './components/Home/Home';
+import NavBar from './components/NavBar/NavBar';
 
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <Router>
+      <div className="App">
+        <Route path="/" component={NavBar}/>
+        <Route exact path="/" component={Home}/>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+
+class NavBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (<> 
+        <p>Navbar Sanity</p>
+         </>);
+    }
+}
+ 
+export default NavBar;
