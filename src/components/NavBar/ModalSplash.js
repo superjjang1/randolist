a2d9500cc759f290ebd17f7d451a7666aac4e1bd
Sean Yim <superjjang89@gmail.com>
2020-01-22 14:54:35 -0500

    modal, navbar styling

---
+++ src/components/NavBar/ModalSplash.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+function ModalSplash(props){
+    return(
+        <div>
+            <button className="spotify-login">Login With Spotify</button>
+        </div>
+    )
+}
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import './NavBar.css';
 import {Link} from 'react-router-dom';
 
 
