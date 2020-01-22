38037071612c7cc4b8cb19dd332e435c6f647d52
Sean Yim <superjjang89@gmail.com>
2020-01-22 14:58:34 -0500

    navbar prep

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 import './NavBar.css';
 import {Link} from 'react-router-dom';
+import ModalSplash from './ModalSplash';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+
 
 
 class NavBar extends Component {
