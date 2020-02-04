67fe9289b9d57dc6616b3e6a87d9308991878c16
Sean Yim <superjjang89@gmail.com>
2020-02-04 13:11:02 -0500

    state changes

--- src/components/Home/SearchBox.js
+++ src/components/Home/SearchBox.js
@@ -2,15 +2,13 @@ import React, {Component} from 'react';
 import './SearchBox.css';
 
 class SearchBox extends Component {
-    state = {};
-    handleArtist=()=>{
-
-    }
-    handleAlbum=()=>{
-
-    }
-    handleSong=()=>{
-
+    state = {
+        search: ""
+    };
+    
+    handleSearch=(e)=>{
+        this.setState({search: e.target.value})
+        console.log(e);
     }
 
     render() { 
@@ -19,7 +17,7 @@ class SearchBox extends Component {
             <div className="input-group-prepend">
                 <span className="input-group-text" id="inputGroup-sizing-lg">Search</span>
             </div>
-            <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg"/>
+            <input onChange={this.handleSearch} type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg"/>
         </div>
         </>);
     }
