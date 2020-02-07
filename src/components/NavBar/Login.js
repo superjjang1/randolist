754aff5917e0aebd2749f50b53d67dfe7cbdda92
Sean Yim <superjjang89@gmail.com>
2020-02-07 15:37:34 -0500

    proper loginModal with spotify auth, well connecting auth still

--- src/components/NavBar/Login.js
+++ src/components/NavBar/Login.js
@@ -1,15 +1,33 @@
 import React, {Component} from 'react';
+import {Button, Box} from '@material-ui/core';
 
 
+const Login = () =>(
+    <Box
+    display="flex"
+    justifyContent="center"
+    alignItems="center"
+    flexDirection="column"
+    style={{ height: "100%" }}
+  >
+    <Box p={1}>
+      <h3 className="logintext"> Discover new music with the help of old favorites. </h3>
+    </Box>
+    <Box p={1}>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => {
+          window.location = `${process.env.REACT_APP_SERVER_URL}/auth/spotify`;
+        }}
+      >
+        Connect with Spotify
+      </Button>
+    </Box>
+  </Box>
+);
 
-class Login extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return (<> <p>sanity</p> </>);
-    }
-}
- 
 export default Login;
+ 
+
+ 
--- src/components/NavBar/ModalSplash.js
+++ src/components/NavBar/ModalSplash.js
@@ -1,10 +1,11 @@
 import React from 'react';
 // import {Link} from 'react-router-dom';
+import Login from './Login';
 
 function ModalSplash(props){
     return(
         <div>
-            <button className="spotify-login text-success">Login With Spotify</button>
+            <Login/>
         </div>
     )
 }
