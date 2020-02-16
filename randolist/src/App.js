4c06a4b5d3809dc59a6fc3fdd3d5b269c4fb76a7
Sean Yim <superjjang89@gmail.com>
2020-02-16 18:02:24 -0500

    new branch

---
+++ randolist/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Home from './components/Home/Home';
+import NavBar from './components/NavBar/NavBar';
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        
+          <Route exact path="/" component={NavBar}/>
+          <Route exact path="/" component={Home}/>
+        
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ randolist/src/components/Home/Home.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+import './Home.css';
+import SearchBox from './SearchBox';
+const queryString = require("query-string");
+const SpotifyWebApi = require("spotify-web-api-node");
+const spotifyApi = new SpotifyWebApi();
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            userInfo: {}
+         };
+    }
+    componentDidMount() {
+        const {location} = this.props;
+        const {access_token} = queryString.parse(location.search);
+        if (!access_token) return;
+        spotifyApi.setAccessToken(access_token);
+        spotifyApi.getMe().then(
+            data=>{
+                this.setState({
+                    userInfo: data.body
+                    
+                });
+            },
+            err =>{
+                console.log("something is wrong",err);
+            }
+        )
+    }
+    render() { 
+        return (<> 
+            <div className="container-fluid">
+                
+                    <h3 className="text-white">Welcome to RandoList {this.state.userInfo.display_name}</h3>
+                    <div className="row">
+                        <div className="home col-sm-8 heroman">
+                            Want to create a quick and EZ playlist?
+                            <div className="col-sm-4 bg-dark text-white spotifyman">
+                                Sign in with your spotify account
+                            </div>
+                            <h1>Hello {this.state.userInfo.display_name}, let's make a playlist!</h1>
+                            
+                            <SearchBox/>
+                        </div>
+                    </div>
+                   
+                        <div className="col-m">
+                            <h1 className="text-white">sanity</h1>
+                        </div>
+                    
+                
+            </div>
+         </>);
+    }
+}
+ 
+export default Home;
