10bf475403085b3dae2fc89e0b5e416a2d48439b
Sean Yim <superjjang89@gmail.com>
2020-02-17 16:41:32 -0500

    changing window.location

--- src/components/NavBar/Login.js
+++ src/components/NavBar/Login.js
@@ -18,7 +18,7 @@ const Login = () =>(
         variant="contained"
         color="primary"
         onClick={() => {
-          window.location = `http://localhost:8010/auth/spotify`;
+          window.location = `http://localhost:8010/login`;
         }}
       >
         Connect with Spotify
