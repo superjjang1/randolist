e66b931a858f6ce3a1ebfcbc41d76c777251ce0c
Sean Yim <superjjang89@gmail.com>
2020-02-24 22:34:54 -0500

    redone appjs/navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,92 @@
-import React from 'react';
-import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import Home from './components/Home/Home';
-import NavBar from './components/NavBar/NavBar';
+import React, { Component } from "react";
+import {BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 
+import Home from "./components/Home/Home";
+import Login from "./components/NavBar/Login";
+import NavBar from "./components/NavBar/NavBar";
+import { ThemeProvider } from "@material-ui/styles";
+import { createMuiTheme } from "@material-ui/core/styles";
 
-function App() {
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#43a047"
+    },
+    secondary: {
+      main: "#43a047"
+    }
+  }
+});
+
+const PrivateRoute = ({ component: Component, isAuthenticated, ...rest }) => {
   return (
-    <Router>
-      <div className="App">
-        
-          <Route exact path="/" component={NavBar}/>
-          <Route exact path="/" component={Home}/>
-        
-      </div>
-    </Router>
+    <Route
+      {...rest}
+      render={props =>
+        isAuthenticated ? <Component {...props} /> : <Redirect to="/login" />
+      }
+    />
   );
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAuthenticated: null,
+      user: null
+    };
+
+    this.getUser = this.getUser.bind(this);
+  }
+
+  componentDidMount() {
+    this.getUser();
+  }
+
+  getUser() {
+    fetch(`http://localhost:8010/auth/login/success`, {
+      credentials: "include"
+    })
+      .then(response => {
+        if (response.status === 200) return response.json();
+        throw new Error("failed to authenticate user");
+      })
+      .then(responseJson => {
+        this.setState({
+          isAuthenticated: true,
+          user: responseJson.user
+        });
+      })
+      .catch(e => {
+        this.setState({
+          isAuthenticated: false,
+          user: null
+        });
+        console.error(e);
+      });
+  }
+
+  render() {
+    const { user, isAuthenticated } = this.state;
+    return (<Router>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <NavBar user={user} />
+          <Switch>
+            <Route path="/login" component={Login} />
+            {isAuthenticated !== null && (
+              <PrivateRoute
+                path="/"
+                isAuthenticated={isAuthenticated}
+                component={Home}
+              />
+            )}
+          </Switch>
+        </div>
+      </ThemeProvider>
+    </Router>);
+  }
 }
 
 export default App;
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,142 +1,185 @@
-import React, {Component} from 'react';
-import './NavBar.css';
-import Login from './Login';
-import ModalSplash from './ModalSplash';
-import {Link} from 'react-router-dom';
+// import React, {Component} from 'react';
+// import './NavBar.css';
+// import Login from './Login';
+// import ModalSplash from './ModalSplash';
+// import {Link} from 'react-router-dom';
 
 
 
 
 
-class NavBar extends Component {
-    state = {
-        showModal: false,
-        modalContent: <ModalSplash/>
-    }
-    componentDidMount(){
-        this.setState({
-            modalContent:<ModalSplash changeModalContent={this.changeModalContent}/>
-        })
-    }
-    login=(e)=>{
-        this.setState({
-            showModal:true
-        })
-    }
-    changeModalContent=(newContent)=>{
-        let modalContent = <ModalSplash changeModalContent={this.changeModalContent}/>
-        if (newContent==='login'){
-            modalContent = <Login changeModalContent={this.changeModalContent} closeModal={this.closeModal}/>
-        }
-        this.setState({modalContent})
-    }
-    closeModal = (e) => {
-        document.querySelector('body').classList = '';
-        this.setState({
-            showModal: false
-        })
-    }
-    // buildNavLinks = () =>{
-    //     let navLinks = 
-    //     <ul id="nav-mobile" className="Right">
-    //             <li>
-    //                 <Link to="#"> hello?</Link>
-    //             </li>
-    //             <li>
-    //                 <Link to="#">Link</Link>
-    //             </li>
-    //             <li>
-    //                 <Link to="#">Link</Link>
-    //             </li>
-    //             <li>
-    //                 <Link to="#">Link</Link>
-    //             </li>
-    //             <li>
-    //                 <Link to="#">Link</Link>
-    //             </li>
-    //         </ul>
+// class NavBar extends Component {
+//     state = {
+//         showModal: false,
+//         modalContent: <ModalSplash/>
+//     }
+//     componentDidMount(){
+//         this.setState({
+//             modalContent:<ModalSplash changeModalContent={this.changeModalContent}/>
+//         })
+//     }
+//     login=(e)=>{
+//         this.setState({
+//             showModal:true
+//         })
+//     }
+//     changeModalContent=(newContent)=>{
+//         let modalContent = <ModalSplash changeModalContent={this.changeModalContent}/>
+//         if (newContent==='login'){
+//             modalContent = <Login changeModalContent={this.changeModalContent} closeModal={this.closeModal}/>
+//         }
+//         this.setState({modalContent})
+//     }
+//     closeModal = (e) => {
+//         document.querySelector('body').classList = '';
+//         this.setState({
+//             showModal: false
+//         })
+//     }
+//     // buildNavLinks = () =>{
+//     //     let navLinks = 
+//     //     <ul id="nav-mobile" className="Right">
+//     //             <li>
+//     //                 <Link to="#"> hello?</Link>
+//     //             </li>
+//     //             <li>
+//     //                 <Link to="#">Link</Link>
+//     //             </li>
+//     //             <li>
+//     //                 <Link to="#">Link</Link>
+//     //             </li>
+//     //             <li>
+//     //                 <Link to="#">Link</Link>
+//     //             </li>
+//     //             <li>
+//     //                 <Link to="#">Link</Link>
+//     //             </li>
+//     //         </ul>
             
         
-    //     // if(!this.props.auth.token){
-    //     //     navLinks = 
-    //     //     <ul id="nav-mobile" className="Right">
-    //     //         <li>
-    //     //             <Link to="#">Link</Link>
-    //     //         </li>
-    //     //         <li>
-    //     //             <Link to="#">Link</Link>
-    //     //         </li>
-    //     //         <li>
-    //     //             <Link to="#">Link</Link>
-    //     //         </li>
-    //     //         <li>
-    //     //             <Link to="#">Link</Link>
-    //     //         </li>
-    //     //         <li>
-    //     //             <Link to="#">Link</Link>
-    //     //         </li>
-    //     //     </ul>
-    //     // }else{
-    //     //     navLinks=
-    //     //     <ul id="nav-mobile" className="right">
-    //     //         <li>
-    //     //             <Link to="#">Link</Link>
-    //     //         </li>
-    //     //         <li>
-    //     //             <Link to="#">Link</Link>
-    //     //         </li>
-    //     //         <li>
-    //     //             <Link to="#">Link</Link>
-    //     //         </li>
-    //     //         <li>
-    //     //             <Link to="#">Link</Link>
-    //     //         </li>
-    //     //         <li>
-    //     //             <Link to="#">Link</Link>
-    //     //         </li>
-    //     //     </ul>
-    //     // }
-    //     // return navLinks
-    // }
-    render() { 
+//     //     // if(!this.props.auth.token){
+//     //     //     navLinks = 
+//     //     //     <ul id="nav-mobile" className="Right">
+//     //     //         <li>
+//     //     //             <Link to="#">Link</Link>
+//     //     //         </li>
+//     //     //         <li>
+//     //     //             <Link to="#">Link</Link>
+//     //     //         </li>
+//     //     //         <li>
+//     //     //             <Link to="#">Link</Link>
+//     //     //         </li>
+//     //     //         <li>
+//     //     //             <Link to="#">Link</Link>
+//     //     //         </li>
+//     //     //         <li>
+//     //     //             <Link to="#">Link</Link>
+//     //     //         </li>
+//     //     //     </ul>
+//     //     // }else{
+//     //     //     navLinks=
+//     //     //     <ul id="nav-mobile" className="right">
+//     //     //         <li>
+//     //     //             <Link to="#">Link</Link>
+//     //     //         </li>
+//     //     //         <li>
+//     //     //             <Link to="#">Link</Link>
+//     //     //         </li>
+//     //     //         <li>
+//     //     //             <Link to="#">Link</Link>
+//     //     //         </li>
+//     //     //         <li>
+//     //     //             <Link to="#">Link</Link>
+//     //     //         </li>
+//     //     //         <li>
+//     //     //             <Link to="#">Link</Link>
+//     //     //         </li>
+//     //     //     </ul>
+//     //     // }
+//     //     // return navLinks
+//     // }
+//     render() { 
        
-        // if(this.props.location.pathname !=='/'){
-        //     navColor="black";
-        // }
-        // const navLinks= this.buildNavLinks();
-        return (
-            <div className="container-fluid">
-            <div className="row">
-                <nav className="navbar navbar-dark  navbar-expand-lg">
-                    <a className="navbar-brand" href="#">RandoList</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle Navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item">
-                                <Link className="nav-link" href="#">Create Playlist</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" href="#" onClick={this.login}>Login</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
+//         // if(this.props.location.pathname !=='/'){
+//         //     navColor="black";
+//         // }
+//         // const navLinks= this.buildNavLinks();
+//         return (
+//             <div className="container-fluid">
+//             <div className="row">
+//                 <nav className="navbar  navbar-expand-lg">
+//                     <a className="navbar-brand text-secondary" href="#">RandoList</a>
+//                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle Navigation">
+//                         <span className="navbar-toggler-icon"></span>
+//                     </button>
+//                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
+//                         <ul className="navbar-nav mr-auto">
+//                             <li className="nav-item">
+//                                 <Link className="nav-link text-secondary" href="#">Create Playlist</Link>
+//                             </li>
+//                             <li className="nav-item">
+//                                 <Link className="nav-link text-secondary" href="#" onClick={this.login}>Login</Link>
+//                             </li>
+//                         </ul>
+//                     </div>
+//                 </nav>
                 
                                
-                <div className="login-modal" style={this.state.showModal ? {"display": "block"} : {}} >
-                    <button id="close-modal" onClick={this.closeModal}>x</button>
-                    <div className="modal-content">
+//                 <div className="login-modal" style={this.state.showModal ? {"display": "block"} : {}} >
+//                     <button id="close-modal" onClick={this.closeModal}>x</button>
+//                     <div className="modal-content">
 
-                    {this.state.modalContent}
-                    </div>
-                </div>
-            </div>
+//                     {this.state.modalContent}
+//                     </div>
+//                 </div>
+//             </div>
 
-        </div>
-            );
-    }
-}
+//         </div>
+//             );
+//     }
+// }
  
-export default NavBar;
+// export default NavBar;
+import React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import ToolBar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+
+const styles = {
+  title: {
+    flex: 1
+  }
+};
+
+const Header = props => {
+  const { user } = props;
+  return (
+    <div>
+      <AppBar position="static">
+        <ToolBar>
+          <Typography type="title" variant="h6" style={styles.title}>
+            RandoList
+          </Typography>
+          {user && (
+            <Button
+              color="inherit"
+              onClick={() => {
+                window.location = `http://localhost:8010/auth/logout`;
+              }}
+            >
+              Logout
+            </Button>
+          )}
+        </ToolBar>
+      </AppBar>
+    </div>
+  );
+};
+
+Header.propTypes = {
+  user: PropTypes.object
+};
+
+export default Header;
